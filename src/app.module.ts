69d5109577c99ff5e33256805e13ef1753436bdf
Gilson Peloso <gilson.peloso@webxnet.com.br>
2023-04-12 18:20:10 -0300

    generate Prisma Client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
     imports: [
+        PrismaModule,
         ConfigModule.forRoot({
             isGlobal: true,
             envFilePath: '.env',
