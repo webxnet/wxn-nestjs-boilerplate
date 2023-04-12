3bd458788fa3cb94e09786a8d731bc91576a6e2e
Gilson Peloso <gilson.peloso@webxnet.com.br>
2023-04-12 18:09:44 -0300

    install @nestjs/config and update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+    const logger = new Logger('Main')
     const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+    const globalPrefix = 'api'
+    const config = app.get<ConfigService>(ConfigService)
+    const port = config.get<number>('SERVER_PORT')
+    const nodeEnv = config.get<string>('NODE_ENV')
+    const basePath = config.get<string>('BASEPATH')
+
+    app.setGlobalPrefix(globalPrefix)
+    await app.listen(port, () => {
+        logger.log(`🚀 Application is running on: ${basePath}:${port}/${globalPrefix}`)
+        logger.log(`Running in mode: ${nodeEnv} `)
+    })
 }
 bootstrap()
